e1316122320c5e9390b62f9d512601490699bc0c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-11 03:57:57 +0900

    command implementation

---
+++ src/main/java/me/TaikiFnit/SpigotPlugin1/CommandKit.java
@@ -0,0 +1,22 @@
+package me.TaikiFnit.SpigotPlugin1;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Player;
+
+public class CommandKit implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player player) {
+
+            ItemStack diamond = new ItemStack(Material.DIAMOND);
+
+            player.getInventory().addItem(diamond);
+        }
+
+        return true;
+    }
+}
--- src/main/java/me/TaikiFnit/SpigotPlugin1/FnitPlugin.java
+++ src/main/java/me/TaikiFnit/SpigotPlugin1/FnitPlugin.java
@@ -1,13 +1,16 @@
+package me.TaikiFnit.SpigotPlugin1;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FnitPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("久しぶりのSpigot Plugin Development!");
+        this.getCommand("fnit").setExecutor(new CommandKit());
     }
 
     @Override
     public void onDisable() {
         getLogger().info("また遊びに来てね！");
     }
-};
+}
